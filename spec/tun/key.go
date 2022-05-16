c47f515f655f3547156350babc86248976026064
Rachel Chen <rachel@chens.email>
2022-05-16 05:06:48 -0700

    spec/tun: include helper functions to generate identities keys; spec/protocol: include tun.IdentitiesPair for forward and backward resolution

--- spec/tun/key.go
+++ spec/tun/key.go
@@ -1,8 +1,22 @@
 package tun
 
-import "strconv"
+import (
+	"strconv"
 
-func Key(hostname string, num int) string {
-	key := strconv.FormatInt(int64(num), 10) + "/" + hostname + "/tun/"
+	"specter/spec/protocol"
+)
+
+func BundleKey(hostname string, num int) string {
+	key := strconv.FormatInt(int64(num), 10) + "/" + hostname + "/bundle/tun/"
+	return key
+}
+
+func IdentitiesChordKey(chord *protocol.Node) string {
+	key := strconv.FormatUint(chord.GetId(), 10) + "/" + chord.GetAddress() + "/identities/chord/"
+	return key
+}
+
+func IdentitiesTunKey(tun *protocol.Node) string {
+	key := strconv.FormatUint(tun.GetId(), 10) + "/" + tun.GetAddress() + "/identities/tun/"
 	return key
 }
