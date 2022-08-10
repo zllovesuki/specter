be9d927e35bd3d69b85e8382257f853a4873bb7e
Rachel Chen <rachel@chens.email>
2022-08-10 01:14:34 -0700

    chord/kv_stats: support printing key value in /_internal endpoint

--- chord/local_trace.go
+++ chord/local_trace.go
@@ -2,15 +2,11 @@ package chord
 
 import (
 	"fmt"
-	"net/http"
 	"sort"
 	"strconv"
 	"strings"
-	"time"
 
 	"kon.nect.sh/specter/spec/chord"
-
-	"github.com/olekukonko/tablewriter"
 )
 
 func minmax(nums []int) (min, max int) {
@@ -87,81 +83,6 @@ func (n *LocalNode) ringTrace() string {
 	}
 
 	return sb.String()
-}
-
-func (n *LocalNode) StatsHandler(w http.ResponseWriter, r *http.Request) {
-	pre, err := n.GetPredecessor()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "error getting predecessor: %v", err)
-		return
-	}
-	succList, err := n.GetSuccessors()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "error getting successor list: %v", err)
-		return
-	}
-	w.Header().Set("content-type", "text/plain")
-
-	fmt.Fprintf(w, "Current state: %s\n", n.state.String())
-	fmt.Fprintf(w, "---\n")
-
-	nodesTable := tablewriter.NewWriter(w)
-	nodesTable.SetHeader([]string{"Where", "ID", "Address"})
-	nodesTable.Append([]string{"Precedessor", fmt.Sprintf("%v", pre.ID()), pre.Identity().GetAddress()})
-	nodesTable.Append([]string{"Local", fmt.Sprintf("%v", n.ID()), n.Identity().GetAddress()})
-
-	for _, succ := range succList {
-		nodesTable.Append([]string{fmt.Sprintf("(Successor; L = %d)", chord.ExtendedSuccessorEntries), fmt.Sprintf("%v", succ.ID()), succ.Identity().GetAddress()})
-	}
-	nodesTable.SetCaption(true, fmt.Sprintf("(stablized: %s)", n.lastStabilized.Load().Round(time.Second).String()))
-	nodesTable.SetAutoMergeCells(true)
-	nodesTable.SetRowLine(true)
-	nodesTable.Render()
-
-	fmt.Fprintf(w, "---\n")
-
-	finger := n.fingerTrace()
-	fingerTable := tablewriter.NewWriter(w)
-	fingerTable.SetHeader([]string{"Range", "ID"})
-	for r, id := range finger {
-		fingerTable.Append([]string{r, id})
-	}
-	fingerTable.SetCaption(true, fmt.Sprintf("(range: %v)", uint64(1<<chord.MaxFingerEntries)))
-	fingerTable.SetAutoMergeCells(true)
-	fingerTable.SetRowLine(true)
-	fingerTable.Render()
-
-	fmt.Fprintf(w, "---\n")
-
-	keysTable := tablewriter.NewWriter(w)
-	keysTable.SetHeader([]string{"hash(key)", "key", "simple", "prefix", "lease"})
-
-	keys := n.kv.RangeKeys(0, 0)
-	exp := n.kv.Export(keys)
-	for i, key := range keys {
-		plain := exp[i].GetSimpleValue()
-		children := exp[i].GetPrefixChildren()
-		lease := exp[i].GetLeaseToken()
-		raw := []any{
-			chord.Hash(key),
-			string(key),
-			plain != nil,
-			len(children),
-			lease,
-		}
-		row := make([]string, len(raw))
-		for i, r := range raw {
-			row[i] = fmt.Sprintf("%v", r)
-		}
-		keysTable.Append(row)
-	}
-	good := kvFsck(n.kv, pre.ID(), n.ID())
-	keysTable.SetCaption(true, fmt.Sprintf("(fsck: %v)", good))
-	keysTable.SetAutoMergeCells(true)
-	keysTable.SetRowLine(true)
-	keysTable.Render()
 }
 
 func kvFsck(kv chord.KVProvider, low, high uint64) bool {
