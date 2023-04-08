e9347d2aacd5402440075881cad5cedff6ac31c2
Rachel Chen <rachel@chens.email>
2023-04-08 00:55:32 -0700

    util/migrator: add simple web ui
    cmd/pki/migrate: removed due to internal migrator endpoint

--- util/migrator/migrator.go
+++ util/migrator/migrator.go
@@ -3,6 +3,7 @@ package migrator
 import (
 	"bytes"
 	"crypto/tls"
+	_ "embed"
 	"net/http"
 
 	"kon.nect.sh/specter/spec/pki"
@@ -19,49 +20,64 @@ type v1ClientConfig struct {
 	Tunnels  []client.Tunnel `yaml:"tunnels,omitempty"`
 }
 
+//go:embed helper.html
+var helper []byte
+
 func ConfigMigrator(logger *zap.Logger, ca tls.Certificate) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		v1Cfg := v1ClientConfig{}
-
-		err := yaml.NewDecoder(r.Body).Decode(&v1Cfg)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
+		switch r.Method {
+		case http.MethodGet:
+			w.Header().Set("content-type", "text/html; charset=utf-8")
+			w.Write(helper)
+		case http.MethodPost:
+			migrateConfig(logger, ca, w, r)
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
 		}
-
-		certPubKey, keyPem := pki.GeneratePrivKey()
-
-		certBytes, err := pki.GenerateCertificate(logger, ca, pki.IdentityRequest{
-			PublicKey: certPubKey,
-			Subject:   pki.MakeSubjectV1(v1Cfg.ClientID, v1Cfg.Token),
-		})
-
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-
-		certPem := pki.MarshalCertificate(certBytes)
-
-		newCfg := client.Config{
-			Version:     2,
-			Apex:        v1Cfg.Apex,
-			Certificate: string(certPem),
-			PrivKey:     keyPem,
-			Tunnels:     v1Cfg.Tunnels,
-		}
-
-		var out bytes.Buffer
-		encoder := yaml.NewEncoder(&out)
-		encoder.SetIndent(2)
-		err = encoder.Encode(&newCfg)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-
-		w.Header().Set("content-type", "application/yaml; charset=utf-8")
-		w.WriteHeader(200)
-		w.Write(out.Bytes())
 	}
+}
+
+func migrateConfig(logger *zap.Logger, ca tls.Certificate, w http.ResponseWriter, r *http.Request) {
+	v1Cfg := v1ClientConfig{}
+
+	err := yaml.NewDecoder(r.Body).Decode(&v1Cfg)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	certPubKey, keyPem := pki.GeneratePrivKey()
+
+	certBytes, err := pki.GenerateCertificate(logger, ca, pki.IdentityRequest{
+		PublicKey: certPubKey,
+		Subject:   pki.MakeSubjectV1(v1Cfg.ClientID, v1Cfg.Token),
+	})
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	certPem := pki.MarshalCertificate(certBytes)
+
+	newCfg := client.Config{
+		Version:     2,
+		Apex:        v1Cfg.Apex,
+		Certificate: string(certPem),
+		PrivKey:     keyPem,
+		Tunnels:     v1Cfg.Tunnels,
+	}
+
+	var out bytes.Buffer
+	encoder := yaml.NewEncoder(&out)
+	encoder.SetIndent(2)
+	err = encoder.Encode(&newCfg)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Header().Set("content-type", "application/yaml; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write(out.Bytes())
 }
