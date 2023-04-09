8598728c5ee34be0c508b16546b48e80c54b36b4
Rachel Chen <rachel@chens.email>
2023-04-08 23:51:16 -0700

    chord: include finger pointer in RingGraph

--- chord/graph_handler.go
+++ chord/graph_handler.go
@@ -3,6 +3,7 @@ package chord
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"kon.nect.sh/specter/spec/chord"
 	"kon.nect.sh/specter/spec/protocol"
@@ -29,13 +30,20 @@ var selfVOptions = append(vOptions,
 	graph.VertexAttribute("color", "lightgrey"),
 )
 
-func RingGraphHandler(root chord.VNode) http.HandlerFunc {
+func RingGraphHandler(root *LocalNode) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var err error
 		var next chord.VNode = root
 
 		nodes := make([]*protocol.Node, 0)
 		seen := make(map[uint64]bool)
+		lookup := make(map[string]*protocol.Node)
+
+		predecessor := root.getPredecessor()
+		if predecessor == nil {
+			http.Error(w, "predecessor is nil", 500)
+			return
+		}
 
 		for {
 			next, err = root.FindSuccessor(chord.ModuloSum(next.ID(), 1))
@@ -69,12 +77,25 @@ func RingGraphHandler(root chord.VNode) http.HandlerFunc {
 			} else {
 				ring.AddVertex(node, vOptions...)
 			}
+			lookup[strconv.FormatUint(node.GetId(), 10)] = node
 		}
 
+		ring.AddEdge(formatNode(root.Identity()), formatNode(predecessor.Identity()), graph.EdgeAttribute("color", "blue"))
 		for i := 0; i < len(nodes)-1; i++ {
-			ring.AddEdge(formatNode(nodes[i]), formatNode(nodes[i+1]))
+			ring.AddEdge(formatNode(nodes[i]), formatNode(nodes[i+1]), graph.EdgeAttribute("color", "red"))
 		}
-		ring.AddEdge(formatNode(nodes[len(nodes)-1]), formatNode(nodes[0]))
+		ring.AddEdge(formatNode(nodes[len(nodes)-1]), formatNode(nodes[0]), graph.EdgeAttribute("color", "red"))
+
+		fingers := root.fingerTrace()
+		for r, id := range fingers {
+			s := formatNode(root.Identity())
+			d := formatNode(lookup[id])
+			err := ring.AddEdge(s, d, graph.EdgeAttribute("label", r))
+			if err == graph.ErrEdgeAlreadyExists {
+				ring.RemoveEdge(s, d)
+				ring.AddEdge(s, d, graph.EdgeAttribute("label", r), graph.EdgeAttribute("color", "red"))
+			}
+		}
 
 		w.Header().Set("content-type", "text/plain")
 		draw.DOT(ring, w)
