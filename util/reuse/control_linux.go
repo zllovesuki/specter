fb0b37a91c5f8b461d7386164b28f1bc8f7466d1
Rachel Chen <rachel@chens.email>
2026-03-10 23:43:01 -0700

    reuse: skip reuse socket options for unix listeners

--- util/reuse/control_linux.go
+++ util/reuse/control_linux.go
@@ -3,12 +3,17 @@
 package reuse
 
 import (
+	"strings"
 	"syscall"
 
 	"golang.org/x/sys/unix"
 )
 
 func Control(network, address string, c syscall.RawConn) error {
+	if !strings.HasPrefix(network, "tcp") && !strings.HasPrefix(network, "udp") {
+		return nil
+	}
+
 	var err error
 	c.Control(func(fd uintptr) {
 		err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
